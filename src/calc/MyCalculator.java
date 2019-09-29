5fd57bf487a730cc09f36cfd3ae3a03da53fdb0f
my <my@172.17.86.25>
2019-09-29 23:19:27 +0530

    author karan

--- src/calc/MyCalculator.java
+++ src/calc/MyCalculator.java
@@ -12,7 +12,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-
+import javax.swing.*;
 
 public class MyCalculator extends Frame implements WindowListener,ActionListener {
 	TextField display;
